a07ee6b26c2ff815d141c0725683da8ac87fccc4
Hongwei_Liu <648699552@qq.com>
2019-10-07 11:38:11 +0800

    Add files via upload

--- R/calibration_plot.R
+++ R/calibration_plot.R
@@ -2,69 +2,101 @@
 #' @description Returns a ggplot2 object of calibration plot
 #' @param y_true Vector of know labels on the test set
 #' @param y_prob Vector of probability predictions on the test set
+#' @param bins If a single number this indicates the number of splits of the data are used to create the
+#' plot. By default, \code{bins} = 10. If a vector, these are actual cuts that will be used.
+#' @param smooths set this to TRUE if you want to draw a smoothing line using \code{\link[stats]{loess}},
+#' or \code{FALSE} to draw a point graph. By default it is \code{FALSE}.
+#' @param conf whether to show 95% confidence bands, by default it is \code{TRUE}.
+#' @param aline whether to connect these points when \code{smooths = FALSE}, by default this is \code{TRUE}
+#' @param point_shape the shape of point, see \code{link[ggplot2]{aes_linetype_size_shape}}, by default 16
+#' @param point_size the size of point, by default it is 3
+#' @param span the parameter α which controls the degree of smoothing
+#' @param errorbar_width the width of the whiskers, by default 0.025
+#' @param errorbar_size the size of the whiskers, by default 0.8  
+#' @return \code{ggplot2 object}
 #' @export
 
-calibration_plot <- function(y_true, y_prob, normalize=FALSE, smooths = F, conf = T){
-  if (!(is.vector(y_true)&is.vector(y_prob)))
-    stop('a')
-  if (normalize){
-    y_prob = (y_prob - min(y_prob)) / (max(y_prob) - min(y_prob))
-
-  }else if(min(y_prob)<0 | max(y_prob) > 1)
-    stop('......')
-  bins = 10
+calibration_plot <- function(x, y, bins = 10, smooths = F, conf = T, abline = F,
+                     point_shape = 16, point_size = 3, span = 0.9, errorbar_width = 0.025, 
+                     errorbar_size = 0.8){
+  #加判断  1.判断输入数据类型
+  #       2.判断输入数据是否符合要求     
+  
   if(length(bins) == 1) {
     num_bins <- bins
     bins <- (0:num_bins)/num_bins
   } else {
-    bins <- unique(c(0, bins, 1))
+    bins <- unique(c(0, bins, 1))  
     num_bins <- length(bins)
   }
-
-  binData <- data.frame(prob = y_prob,
-                        bin = cut(y_prob, bins, include.lowest = TRUE),
-                        class = y_true)
-
-  dataPoints <- ddply(binData,
-                      .(bin),
-                      function(x) {
-                        if(nrow(x) > 0) {
-                          tmp <- binom.test(x = sum(x$class == 1), n = nrow(x))
-                          out <- c(Percent = mean(x$class == 1),
-                                   Lower  = tmp$conf.int[1],
-                                   Upper  = tmp$conf.int[2],
-                                   Count = sum(x$class == 1))
-                        } else out <- c(Percent = NA, Lower  = NA,
-                                        Upper  = NA, Count = 0)
-                        out
-                      },
-                      .drop = FALSE)
-
-  dataPoints$midpoint <- NA
-  for(i in 2:length(bins))
-    dataPoints$midpoint[i-1] <- .5*(bins[i] + bins[i-1])
-  dataPoints$Percent <- ifelse(dataPoints$Count == 0, 0, dataPoints$Percent)
-
-
-  p <- ggplot(dataPoints, aes(x = midpoint, y = Percent)) +
-    geom_abline(slope = 1, intercept = 0, col = "red", lty = 2, alpha = .6) +
+  x <- data.frame(x)
+  # x must be a data.frame
+  binData <- data.frame(prob = x,
+                        bin = apply(x, 2,cut, breaks = bins, include.lowest = TRUE),
+                        class = y)
+  # draw a empty diagram
+  p <- ggplot() +
+    geom_abline(slope = 1, intercept = 0, col = "black", lty = 2, alpha = .6) +
     theme(panel.background = element_blank(),
           aspect.ratio = 0.7,
           plot.margin = margin(30,0,30,0),
-          panel.grid.major.y = element_line(colour = "grey", linetype = 2, size = 0.1),
+          panel.grid.major.y = element_line(colour = "grey", linetype = 2, size = 0.1), 
           axis.line = element_line(color = "black"),
           axis.title.x = element_text(vjust = 0.1),
-          axis.title.y = element_text(vjust = 1)) +
-    scale_x_continuous(name="Predicted probability", limits=c(0,1), breaks = seq(0,1,0.1), expand = expand_scale(add = 0)) +
-    scale_y_continuous(name = "Observed proportion", limits = c(0,1), breaks = seq(0,1,0.1), expand = expand_scale(add = 0))
-
-  if(smooths == T ){
-    p <- p + geom_smooth(method = "loess", span = 0.9, se = conf)
-  }else if(conf == T){
-    p <- p + geom_point() +
-      geom_errorbar(aes(ymin = Lower, ymax = Upper), width=0.025, colour = "black", size=0.8)
-  }else{
-    p <- p + geom_point()
+          axis.title.y = element_text(vjust = 1),
+          legend.title=element_blank()) +
+    scale_x_continuous(name="Predicted probability", limits=c(0,1.05), breaks = seq(0,1,0.1), expand = expand_scale(add = 0)) + 
+    scale_y_continuous(name = "Observed proportion", limits = c(0,1.05), breaks = seq(0,1,0.1), expand = expand_scale(add = 0))
+  
+  # add element 
+  model_num <- ncol(x)
+  
+  for (i in 1:model_num) {
+    data_tmp <- binData[,c(i,i + model_num,ncol(binData))]
+    colnames(data_tmp) <- c("prob","bin","class")
+    dataPoints_tmp <- ddply(data_tmp,
+                            .(bin),
+                            function(x) {
+                              if(nrow(x) > 0) {
+                                tmp <- binom.test(x = sum(x$class == 1), n = nrow(x))
+                                out <- c(Observed = mean(x$class == 1),
+                                         Lower  = tmp$conf.int[1],
+                                         Upper  = tmp$conf.int[2],
+                                         Estimated = mean(x$prob))
+                              } else out <- c(Observed = NA, Lower  = NA,
+                                              Upper  = NA, Estimated = 0)
+                              out
+                            },
+                            .drop = FALSE)
+    
+    if(smooths == T ){
+      p <- p + geom_smooth(method = "loess", span = span, se = conf,
+                           aes(x = Estimated, y = Observed), data = dataPoints_tmp)
+    }else if(conf == T){
+      if (abline == T){
+        p <- p + geom_point(shape = point_shape, size = point_size,
+                            aes(x = Estimated, y = Observed),color = i, data = dataPoints_tmp) + 
+          geom_errorbar(aes(ymin = Lower, ymax = Upper, x = Estimated), 
+                        data = dataPoints_tmp, width= errorbar_width, colour = i, 
+                        size= errorbar_size) + 
+          geom_line(linetype = 1, size = 1, colour = i, aes(x = Estimated, y = Observed),
+                    data = dataPoints_tmp)
+      }else{
+        p <- p + geom_point(shape = point_shape, size = point_size, color = i,
+                            aes(x = Estimated, y = Observed), data = dataPoints_tmp) + 
+          geom_errorbar(aes(ymin = Lower, ymax = Upper, x = Estimated), 
+                        data = dataPoints_tmp, width= errorbar_width, colour = i, 
+                        size= errorbar_size) 
+      }
+    }else if (abline == T){
+      p <- p + geom_point(shape = point_shape, size = point_size, color = i,
+                          aes(x = Estimated, y = Observed), data = dataPoints_tmp) + 
+        geom_line(linetype = 1, colour = i, size = 1, aes(x = Estimated, y = Observed),
+                  data = dataPoints_tmp)
+    }else {
+      p <- p + geom_point(shape = point_shape, size = point_size, color = i,
+                          aes(x = Estimated, y = Observed), data = dataPoints_tmp)
+    }
   }
   return(p)
 }
